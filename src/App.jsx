b94e966b588b4936e2ea181f3602716e62121cca
seyitan001 <99769210+seyitan001@users.noreply.github.com>
2024-12-09 19:49:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Todos from "./Components/Todos.jsx";
+import AddTodo from "./Components/AddTodo.jsx";
+function App() {
+ return(<>
+      <AddTodo/>
+      <Todos/>
+ </>
+
+ )
+}
+
+export default App
