0feb6aac7635a349b42fde5e33b213d75e61306f
seyitan001 <99769210+seyitan001@users.noreply.github.com>
2024-12-12 09:52:31 +0100

    Hello

--- src/features/todos/todoSlice.js
+++ src/features/todos/todoSlice.js
@@ -1,1 +1,57 @@
-import {createSlice, nanoid} from "@reduxjs/toolkit";const initialState ={    todos:[],}export const todoSlice = createSlice({    name:"todos",    initialState,    reducers:{        addTodo:(state,action)=>{           const todo ={               id:nanoid(),               text: action.payload,               completed:false           }           state.todos.push(todo);        },        removeTodo:(state,action) =>{           state.todos = state.todos.filter(todo => todo.id !== action.payload)        },        updateTodo: (state,action) => {            const {id , text} = action.payload            console.log(id, text)          state.todos=  state.todos.map(todo => {                if(todo.id === id){                    return {...todo, text: text, form: true}                }                return todo;            })        },        markCompleted:(state,action) => {            state.todos.map(todo => todo.id === action.payload.id ?                {...todo , completed: !todo.completed} : todo)        },    }})export const {addTodo, removeTodo, updateTodo, markCompleted, } = todoSlice.actions;export default todoSlice.reducer//slice is responsible for the initial state//and the reducer function//reducer is responsible for updating the state//nanoid is used to generate unique id it is from redux toolkit automatically//create an initialState//create a slice for your objects, pass in the initial state, and the reducers//create each reducers//export all the reducers as actions//export the slice.reducer//go back to your store
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+        completed: false,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      console.log(id, text);
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, text: text, form: true };
+        }
+        return todo;
+      });
+    },
+    markCompleted: (state, action) => {
+      state.todos.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo, markCompleted } =
+  todoSlice.actions;
+
+export default todoSlice.reducer;
+
+//slice is responsible for the initial state
+//and the reducer function
+//reducer is responsible for updating the state
+//nanoid is used to generate unique id it is from redux toolkit automatically
+
+//create an initialState
+//create a slice for your objects, pass in the initial state, and the reducers
+//create each reducers
+//export all the reducers as actions
+//export the slice.reducer
+//go back to your store
